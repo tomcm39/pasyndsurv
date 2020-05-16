356b569f8937af3569bcf2bf5568a74678d37f9d
tom <tomcm39@gmail.com>
2020-05-16 12:38:40 -0400

    moving downloadHelper package

--- data/deaths/downloadHelper/cases.py
+++
@@ -1,6 +0,0 @@
-#mcandrew;
-
-def timestamp():
-    from datetime import datetime
-    time = datetime.now()
-    return "{:04d}-{:02d}-{:02d}-{:02d}".format(time.year,time.month,time.day,time.hour)
