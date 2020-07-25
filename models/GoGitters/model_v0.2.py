da3237ea0f1900d3e0b7f3da8ebd1b2db66ffb49
michaelonelson <monelson@uvm.edu>
2020-07-24 21:36:26 -0400

    Update model_v0.2.py
    change to run through all counties instead of one specific one

--- models/GoGitters/model_v0.2.py
+++ models/GoGitters/model_v0.2.py
@@ -1,17 +1,5 @@
 #mcandrew, nelson, liu
 
-#new python script in folder
-#load in pa training data
-#load in census data
-#create var N cumulative number of new positives new positives / total people in census
-#create var C cumulative new pos / census
-#to proportion of people in census – number of new positives/ number of people in census
-
-#S = 1-C
-
-#plot some C vars of different fips
-#matplotlib
-
 import sys
 import numpy as np
 import pandas as pd
@@ -20,22 +8,28 @@ import seaborn as sns
 
 if __name__ == "__main__":
     allData = pd.read_csv("data/cases/PATrainingDataCases.csv")
+# iterating the columns 
+    for col in allData.columns: 
+        print(col) 
+
 
     #subset data to the most recent training week and a single county
-    mostrecentweek = allData.trainingweek.max()
-    singleCounty = allData[ (allData.fips == 42091) & (allData.trainingweek==mostrecentweek)  ]
-
-    newPos = singleCounty.dohweb__numnewpos
-    census = singleCounty.census
-    totPos = np.cumsum(newPos)
+    for (fips,subsetdata) in allData.groupby(["fips"]):
+        newPos = subsetdata.dohweb__numnewpos
+        census = subsetdata.census
+        totPos = np.cumsum(newPos)
  
-    #create var N: new cases per population
-    N = newPos/census
+        #create var N: new cases per population
+        N = newPos/census
 
-    #create var C: cumulative cases per population
-    C = totPos/census
+        #create var C: cumulative cases per population
+        C = totPos/census
     
-    #create var S : cumulative non-cases per population
-    S = 1 - C
+        #create var S : cumulative non-cases per population
+        S = 1 - C
 
-    print(N, C, S)
+        print(fips)
+        print(N)
+        print(C)
+        print(S)
+
