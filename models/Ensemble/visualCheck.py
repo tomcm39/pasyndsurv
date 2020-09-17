f50c5c7c1d2c83d36a2619539ac63d34601e9852
Damon Luk <chl221@lehigh.edu>
2020-09-17 17:16:18 -0400

    new code

--- models/Ensemble/visualCheck.py
+++ models/Ensemble/visualCheck.py
@@ -1,26 +1,27 @@
-luk, donnachie, mcandrew
+#luk, donnachie, mcandrew
 
 import seaborn as sns
 import matplotlib.pyplot as plt
-
+import model_v02
+ 
 #from McAndrew's model_v0.2 notes
 # ----- code for plotting ---------------------------------------------------------------------------
     # let's move this code to a separate python script called visualCheck.py insidet our same folder
+d = model_v02.singleEWForecast
+d = d [ (d.forecastTW == 202024) ]
 
-    sns.style.use("fivethirtyright")
+sns.style.use("fivethirtyright")
 
-    fig,ax = plt.subplots()
-
-    d = singleEWForecast
-    ax.plot( d.numnewcases_mid, d.prob, "b-", linewidth=2 )
+fig,ax = plt.subplots()
+ax.plot( d.numnewcases_mid, d.prob, "b-", linewidth=2 )
     
-    ax.set_xlabel("Number of new cases", fontsize=10)
-    ax.set_xlabel("PMF", fontsize=10)
+ax.set_xlabel("Number of new cases", fontsize=10)
+ax.set_xlabel("PMF", fontsize=10)
 
-    ax.tick_params(size=2.,direction="in")
+ax.tick_params(size=2.,direction="in")
 
-    ax.text(0.99,0.99,"Equally Weighted Ensemble"
-            ,ha='right',va='top',transform=ax.transAxes)
+ax.text(0.99,0.99,"Equally Weighted Ensemble"
+        ,ha='right',va='top',transform=ax.transAxes)
 
-    plt.savefig("visualCheckProbDist.pdf")
-    plt.close()
+plt.savefig("visualCheckProbDist.pdf")
+plt.close()
