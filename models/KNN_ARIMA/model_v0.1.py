c2d96cb2341cab521e64deb3f1b49c94a6fbd8c7
tom <tomcm39@gmail.com>
2020-10-01 14:11:27 -0400

    KNN ARIMA

--- models/KNN_ARIMA/model_v0.1.py
+++ models/KNN_ARIMA/model_v0.1.py
@@ -6,9 +6,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
+import scipy
 from statsmodels.tsa.arima_model import ARIMA # note the import here (you may need to pip3 install statsmodel)
-
 
 if __name__ == "__main__":
 
@@ -76,17 +75,16 @@ if __name__ == "__main__":
             
             D[i,j] = euclideanDistance( fipItimeSeries.dohweb__numnewpos, fipJtimeSeries.dohweb__numnewpos )
     
-    # why are all the diagonal entries of D zero? Seems weird, or does it?
-
-    
     # now we will build a forecast for each of the 5 closest counties to our "target" county.
     
-    K=5
+    K=5 # NUMNBER OF NEIGHBORS
     KclosestCounties = np.argsort( D[:, FIPS2index[targetFIP]] )[:K] # take a look at what argsort does here = https://numpy.org/devdocs/reference/generated/numpy.argsort.html
 
     closestFIPS = [ index2FIPS[index] for index in KclosestCounties]
     
-    fourWeekAheadForecasts = np.zeros( (K,5) ) # 5 closest time series by 4 week ahead forecasts (plus a 0 week ahead forecast)
+    stepsize = 5
+    probBins = np.arange(0,5*10**2,stepsize)
+    distOfForecasts = np.zeros( (K, len(probBins),4) )
     for i,fip in enumerate(closestFIPS):
         fipData = singleTrainingWeek[ singleTrainingWeek.fips==fip ] # take a single FIPS worth of data
         fipData = fipData[['modelweek','dohweb__numnewpos']].set_index(['modelweek']) #  only need these two columns
@@ -94,46 +92,27 @@ if __name__ == "__main__":
         # build an ARIMA model
         # Documentation for ARIMA from Statmodels is here
         # https://www.statsmodels.org/stable/generated/statsmodels.tsa.arima_model.ARIMA.html
-        arimaModel = ARIMA( fipData, order=(1,0,0) )
+        arimaModel = ARIMA( fipData, order=(1,0,1) )
         fittedModel = arimaModel.fit()
 
         lastWeekOfData = fipData.index.max()
-        forecasts = fittedModel.predict(start=len(fipData),end=len(fipData)+4) # some issues will arise here when we are in december
+        mn,stderr,confint = fittedModel.forecast(steps=4)
         
-        fourWeekAheadForecasts[i,:] = forecasts
-        
-    # lets take the average forecast from all 5 models as the forecasts for our county.
-    mostRecentModelWeek = fipData.index.max() # the index is modelweek
-    avgForecast = fourWeekAheadForecasts.mean(0)
+        for weekahead in np.arange(1,4+1,1):
+            dist = scipy.stats.norm(loc = mn[weekahead-1] , scale = stderr[weekahead-1] )
+            
+            probs = [ float(dist.cdf(numOfNewCases+stepsize) - dist.cdf(numOfNewCases) ) for numOfNewCases in np.arange(0,5*10**2,stepsize)]
+            distOfForecasts[i,:,weekahead-1] = probs
+            
+    distOfForecasts = distOfForecasts.mean(axis=0) # average K prob dists
+    distOfForecasts = distOfForecasts / distOfForecasts.sum(0) # normalize prob dists
 
-    fig,ax = plt.subplots()
-
-    ax.plot( singleCounty.modelweek, singleCounty.dohweb__numnewpos, color='blue',alpha=0.70 )
-
-    ax.plot( np.arange(mostRecentModelWeek,mostRecentModelWeek+4+1) , avgForecast, color ='red',alpha=0.80, label = "Avg Forecast")
-    for i,closeCountyForecast in enumerate(fourWeekAheadForecasts):
-        if i==0:
-            clr='purple'
-            lbl="Target FIP forecast"
-            alpha=1.0
-        elif i==1:
-            clr='gray'
-            lbl = "Close County Forecast"
-            alpha=0.5
-        else:
-            clr="gray"
-            lbl="_none_"
-            alpha=0.5
-        ax.plot( np.arange(mostRecentModelWeek,mostRecentModelWeek+4+1) , closeCountyForecast, color =clr,alpha=alpha, label = lbl)
-
-    ax.set_xlabel("Model week (number of epiweeks since 1970W1)")
-    ax.set_ylabel("Number of new COVID-19 cases")
-
-    ax.legend()
-    
-    plt.show()
-
-    # TODO
-    # these forecasts are POINT forecasts. We need a proabbilistic distribuion over the number of new cases.
-    # lets think about how to do that
-    
+    nBins = len(probBins)
+    allForecastData = pd.DataFrame()
+    for weekahead in np.arange(1,4+1,1):
+        forecastData = { 'numnewcases_leftbin': probBins
+                         ,'numnewcases_rightbin':np.arange(stepsize,5*10**2+stepsize,stepsize)
+                         ,'weekahead':[weekahead]*nBins
+                         ,'prob':distOfForecasts[:,weekahead-1]}
+        allForecastData = allForecastData.append( pd.DataFrame(forecastData)  )
+    return allForecastData
